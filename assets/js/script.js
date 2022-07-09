27f68aa9848e1414da03b48dab0b60bff3210f1b
Lil Ferry <shawney134@gmail.com>
2022-07-09 10:43:19 -0400

    made code a bit cleaner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,25 @@
-//  
+document.querySelector("#currentDay").textContent = moment().format("MMMM Do, YYYY, h:mm a");  // function to display current date using moment.js
 var currentHour = moment().hour(); //(moment().hour() returns current hour in 12 hour format) 
 var saveBtn = document.querySelectorAll(".saveBtn"); // save button  
 
-
-// function to display current date using moment.js
-document.querySelector("#currentDay").textContent = moment().format("MMMM Do, YYYY, h:mm a");  
 
 // switch case to display the current text for the appropriate time block and set the background color accordingly (past, present, future) (9am - 5pm) 
 for ( var i = 0; i < 9; i++) {
   switch (i) {
     case 0:
-        document.getElementById(i).classList.add("past");
+        document.getElementById(i).classList.add("past");   // past class
         break;
     case 1:
-        document.getElementById(i).classList.add("past");
+        document.getElementById(i).classList.add("past");   // past class
         break;
     case 2:
-        document.getElementById(i).classList.add("past");
+        document.getElementById(i).classList.add("past");   // past class
         break;
     case 3: 
-        document.getElementById(i).classList.add("present");
+        document.getElementById(i).classList.add("present");    // present class
         break;
     case i:
-        document.getElementById(i).classList.add("future");
+        document.getElementById(i).classList.add("future");   // future class
         break; 
  }
 
